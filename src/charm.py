8ce73c93aa6d486d4a239f5b35dea636f7f6786c
Aymen Frikha <aymen.frikha@canonical.com>
2021-11-23 10:41:41 +0000

    change design for intereaction between principal charm and pgbouncer-extra-config

--- src/charm.py
+++ src/charm.py
@@ -3,12 +3,16 @@
 # See LICENSE file for licensing details.
 
 
+import jinja2
+import json
 import logging
-
+import os
 from ops.charm import CharmBase
 from ops.main import main
 from ops.framework import StoredState
 from ops.model import MaintenanceStatus, ActiveStatus, BlockedStatus
+from charmhelpers.core import hookenv, host
+import uuid
 
 
 logger = logging.getLogger(__name__)
@@ -30,9 +34,6 @@ class CharmPgbouncerExtraConfig(CharmBase):
             self.on.upgrade_charm,
             self._on_config_changed_or_upgrade)
         self.framework.observe(
-            self.on.pgbouncer_extra_config_relation_joined,
-            self._on_config_changed_or_upgrade)
-        self.framework.observe(
             self.on.pgbouncer_extra_config_relation_changed,
             self._on_config_changed_or_upgrade)
 
@@ -51,21 +52,49 @@ class CharmPgbouncerExtraConfig(CharmBase):
 
         charm_config = self.framework.model.config
         remote = self.framework.model.relations.get('pgbouncer-extra-config')[0]
+        sections_extra_config = {"databases": [], "pgbouncer": []}
+        auth_user_dir = "/etc/pgbouncer/auth_user/"
+        extra_dbs_dir = "/etc/pgbouncer/extra_dbs/"
         for unit in self._rel_get_remote_units('pgbouncer-extra-config'):
             if 'extra_db_config' in charm_config:
-                remote.data[self.unit]['extra_db_config'] = \
-                    charm_config['extra_db_config']
+                self.prepare_folder(extra_dbs_dir)
+                extra_dbs_conf = extra_dbs_dir + str(uuid.uuid4()) + '.ini'
+                self.render_config(extra_dbs_conf,
+                                   'extra_databases.ini.tmpl')
+                sections_extra_config["databases"].append(extra_dbs_conf)
+
             if 'auth_user' in charm_config:
-                remote.data[self.unit]['auth_user'] = \
-                    charm_config['auth_user']
                 if 'auth_query' in charm_config:
-                    remote.data[self.unit]['auth_query'] = \
-                        charm_config['auth_query']
+                    self.prepare_folder(auth_user_dir)
+                    auth_user_conf = auth_user_dir + str(uuid.uuid4()) + '.ini'
+                    self.render_config(auth_user_conf, 'auth_user.ini.tmpl')
+                    sections_extra_config["pgbouncer"].append(auth_user_conf)
                 else:
-                    self.unit.status = BlockedStatus("auth_query needs"\
-                    " to be configured when auth_user is set")
+                    self.unit.status = BlockedStatus(
+                        "auth_query needs to be configured when auth_user is set")
                     return
+            remote.data[self.unit]['section_extra_parameters'] = json.dumps(sections_extra_config)
+
         self.unit.status = ActiveStatus("Unit is ready")
+
+    def render_config(self, path, tmpl_name):
+        """Render extra configuration files"""
+        loader = jinja2.FileSystemLoader(
+            os.path.join(hookenv.charm_dir(), 'src/templates'))
+        env = jinja2.Environment(loader=loader)
+        env.globals['config'] = self.framework.model.config
+        template = env.get_template(tmpl_name)
+        contents = template.render()
+        if not os.path.exists(path):
+            open(path, 'a').close()
+        if contents.encode() != open(path, 'rb').read():
+            host.write_file(path, contents.encode())
+
+    def prepare_folder(self, dir):
+        if not os.path.exists(dir):
+            os.makedirs(dir)
+        for f in os.listdir(dir):
+            os.remove(os.path.join(dir, f))
 
 
 if __name__ == "__main__":
--- unit_tests/test_charm.py
+++ unit_tests/test_charm.py
@@ -2,10 +2,13 @@
 # See LICENSE file for licensing details.
 
 import unittest
+import json
+from unittest.mock import Mock, patch
 from ops.model import Relation, BlockedStatus
 from ops.testing import Harness
 from src.charm import CharmPgbouncerExtraConfig
 
+CHARM = "src.charm.CharmPgbouncerExtraConfig"
 
 
 class TestCharm(unittest.TestCase):
@@ -19,35 +22,38 @@ class TestCharm(unittest.TestCase):
         self.pooler = \
             self.harness.add_relation('pgbouncer-extra-config', 'pgbouncer')
         self.harness.add_relation_unit(self.pooler, 'pgbouncer/0')
-       
+
+    @patch(f"{CHARM}.prepare_folder", Mock(return_value=True))
+    @patch(f"{CHARM}.render_config", Mock(return_value=True))
     def test_config_changed(self):
-        conf_dict = {
-          "extra_db_config": "mydb_primary = \
-           host=125.25.32.52  port=5432 dbname=mydb",
-          "auth_user": "test_auth",
-          "auth_query": "SELECT usename, passwd FROM \
-           pg_shadow WHERE usename = $1 ;"
-        }
+        conf_dict = {"extra_db_config": "mydb_primary = \
+                     host=125.25.32.52  port=5432 dbname=mydb",
+                     "auth_user": "test_auth",
+                     "auth_query": "SELECT usename, passwd FROM \
+                     pg_shadow WHERE usename = $1 ;"
+                     }
         self.harness.update_config(conf_dict)
         rel = self.model.get_relation('pgbouncer-extra-config', 0)
         self.assertIsInstance(rel, Relation)
-        self.assertEqual(
-            rel.data[self.model.unit], conf_dict)
+        print(rel.data[self.model.unit]["section_extra_parameters"][0])
+        self.assertEqual(len(json.loads(rel.data[self.model.unit]
+                             ["section_extra_parameters"])['databases']), 1)
+        self.assertEqual(len(json.loads(rel.data[self.model.unit]
+                             ["section_extra_parameters"])['pgbouncer']), 1)
 
+    @patch(f"{CHARM}.prepare_folder", Mock(return_value=True))
+    @patch(f"{CHARM}.render_config", Mock(return_value=True))
     def test_blocked_status(self):
         self.harness.update_config(unset=["auth_query"])
-        conf_dict = {
-          "extra_db_config": "mydb_primary = \
-           host=125.25.32.52  port=5432 dbname=mydb",
-          "auth_user": "test_auth",
-        }
+        conf_dict = {"extra_db_config": "mydb_primary = \
+                     host=125.25.32.52  port=5432 dbname=mydb",
+                     "auth_user": "test_auth",
+                     }
         self.harness.update_config(conf_dict)
         self.harness.charm.on.update_status.emit()
         self.assertEqual(
             self.harness.charm.unit.status.message,
-            "auth_query needs"\
-             " to be configured when auth_user is set")
+            "auth_query needs to be configured when auth_user is set")
         self.assertIsInstance(
             self.harness.charm.unit.status,
             BlockedStatus)
-
